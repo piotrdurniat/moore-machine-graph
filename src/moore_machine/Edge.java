2937f4a7a6dcdbcdce9c97b947e740ee9ea91a36
Piotr Durniat <piotr@durniat.com>
2020-12-03 18:06:34 +0100

    feat: added curved edges

--- src/moore_machine/Edge.java
+++ src/moore_machine/Edge.java
@@ -2,10 +2,19 @@ package moore_machine;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.geom.QuadCurve2D;
+import java.awt.BasicStroke;
+import java.awt.Stroke;
 
 class Edge {
     protected Node startNode;
     protected Node endNode;
+
+    protected int height = 100;
+
+    boolean mouseOver = false;
 
     Edge(Node startNode, Node endNode) {
         setStartNode(startNode);
@@ -33,64 +42,93 @@ class Edge {
     }
 
     protected double[] getStartPoint() {
+        double sx = getPeakPos()[0];
+        double sy = getPeakPos()[1];
+        double ex = startNode.getX();
+        double ey = startNode.getY();
 
-        double sx = startNode.getX();
-        double sy = startNode.getY();
-        double ex = endNode.getX();
-        double ey = endNode.getY();
-
-        double xDiff = sx - ex;
-        double yDiff = sy - ey;
-
-        int r = startNode.getR();
-
-        double dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-
-        double x = (sx - (r * xDiff) / dist);
-        double y = (sy - (r * yDiff) / dist);
-
-        double[] point = { x, y };
-        return point;
+        return getIntersectWithCircle(sx, sy, ex, ey);
     }
 
     protected double[] getEndPoint() {
-
-        double sx = startNode.getX();
-        double sy = startNode.getY();
+        double sx = getPeakPos()[0];
+        double sy = getPeakPos()[1];
         double ex = endNode.getX();
         double ey = endNode.getY();
 
+        return getIntersectWithCircle(sx, sy, ex, ey);
+    }
+
+    private double[] getIntersectWithCircle(double sx, double sy, double ex, double ey) {
         double xDiff = ex - sx;
         double yDiff = ey - sy;
-
         int r = endNode.getR();
 
         double dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-
         double x = (ex - (r * xDiff) / dist);
         double y = (ey - (r * yDiff) / dist);
-
         double[] point = { x, y };
         return point;
-    }
-
-    boolean isMouseOver(int mx, int my) {
-        double sx = startNode.getX();
-        double sy = startNode.getY();
-        double ex = endNode.getX();
-        double ey = endNode.getY();
-        return linePoint(sx, sy, ex, ey, mx, my);
     }
 
     boolean linePoint(double x1, double y1, double x2, double y2, double px, double py) {
         double d1 = dist(px, py, x1, y1);
         double d2 = dist(px, py, x2, y2);
         double lineLen = dist(x1, y1, x2, y2);
-        double buffer = 0.2; // higher # = less accurate
+        double buffer = 0.2;
         if (d1 + d2 >= lineLen - buffer && d1 + d2 <= lineLen + buffer) {
             return true;
         }
         return false;
+    }
+
+    private void drawArc(Graphics g, int sx, int sy, int px, int py, int ex, int ey) {
+        Graphics2D g2d = (Graphics2D) g;
+        QuadCurve2D q = new QuadCurve2D.Float();
+
+        q.setCurve(sx, sy, px, py, ex, ey);
+        g2d.draw(q);
+    }
+
+    protected double[] getPeakPos() {
+        double sx = startNode.getX();
+        double sy = startNode.getY();
+        double ex = endNode.getX();
+        double ey = endNode.getY();
+        double angle = Math.atan2(ey - sy, ex - sx);
+
+        double[] middle = getMiddlePoint(sx, sy, ex, ey);
+
+        double peakX = middle[0] + height * Math.cos(angle - Math.PI / 2);
+        double peakY = middle[1] + height * Math.sin(angle - Math.PI / 2);
+
+        double[] peak = { peakX, peakY };
+        return peak;
+    }
+
+    protected double[] getMiddlePoint(double sx, double sy, double ex, double ey) {
+        double middleX = (sx + ex) / 2;
+        double middleY = (sy + ey) / 2;
+        double[] middle = {middleX, middleY};
+        return middle;
+    }
+
+    private void drawArrowHead(Graphics g, double x1, double y1, double x2, double y2) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        Polygon arrowHead = new Polygon();
+        arrowHead.addPoint(0, 5);
+        arrowHead.addPoint(-5, -5);
+        arrowHead.addPoint(5, -5);
+
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        g2d.translate(x2, y2);
+        g2d.rotate((angle - Math.PI / 2d));
+
+        g2d.fill(arrowHead);
+
+        g2d.rotate(-(angle - Math.PI / 2d));
+        g2d.translate(-x2, -y2);
     }
 
     private double dist(double x1, double y1, double x2, double y2) {
@@ -101,12 +139,82 @@ class Edge {
 
     void draw(Graphics g) {
         g.setColor(Color.BLACK);
-        g.drawLine((int) getStartPoint()[0], (int) getStartPoint()[1], (int) getEndPoint()[0], (int) getEndPoint()[1]);
+
+        int sx = (int) getStartPoint()[0];
+        int sy = (int) getStartPoint()[1];
+        int ex = (int) getEndPoint()[0];
+        int ey = (int) getEndPoint()[1];
+
+        int px = (int) getPeakPos()[0];
+        int py = (int) getPeakPos()[1];
+
+        drawArc(g, sx, sy, px, py, ex, ey);
+        drawArrowHead(g, px, py, ex, ey);
+        g.drawOval(px, py, 10, 10);
+
+        drawEnclosingRect(g);
+    }
+
+    protected void drawEnclosingRect(Graphics g) {
+        if(!mouseOver) return;
+    
+        Graphics2D g2d = (Graphics2D) g;
+
+        Stroke dashed = new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+        g2d.setStroke(dashed);
+        g.setColor(Color.RED);
+
+        Polygon rect = enclosingRect();
+        g2d.draw(rect);
+
+        g2d.setStroke(new BasicStroke());
     }
 
     @Override
     public String toString() {
         return startNode + "-" + endNode;
     }
+
+    protected Polygon enclosingRect() {
+        int sx = (int) getStartPoint()[0];
+        int sy = (int) getStartPoint()[1];
+        int ex = (int) getEndPoint()[0];
+        int ey = (int) getEndPoint()[1];
+
+        double[] m = getMiddlePoint(sx, sy, ex, ey);
+        double[] p = getPeakPos();
+        int distY = (int) dist(m[0], m[1], p[0], p[1]);
+
+        Polygon rect = new Polygon();
+
+        double angle = Math.atan2(ey - sy, ex - sx);
+
+        rect.addPoint(sx, sy);
+        rect.addPoint(ex, ey);
+
+        int diffX = (int) (Math.cos(angle - Math.PI / 2) * distY);
+        int diffY = (int) (Math.sin(angle - Math.PI /2) * distY);
+
+        rect.addPoint(ex + diffX, ey + diffY);
+        rect.addPoint(sx + diffX, sy + diffY);
+        return rect;
+    }
+
+    boolean isMouseOver(int mx, int my) {
+        Polygon rect = enclosingRect();
+
+        if(rect.contains(mx, my)) {
+            mouseOver = true;
+            return true;
+        }
+        mouseOver = false;
+        return false;
+    }
+
+    public void move(double x, double y) {
+        startNode.move(x, y);
+        endNode.move(x, y);
+    }
+
 
 }
--- src/moore_machine/MooreEdge.java
+++ src/moore_machine/MooreEdge.java
@@ -1,35 +1,14 @@
 package moore_machine;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Polygon;
 import java.awt.Font;
 import java.awt.Color;
 
 import java.awt.FontMetrics;
 
-
 public class MooreEdge extends Edge {
 
     private String input;
-
-    private void drawArrowHead(Graphics g, double x1, double y1, double x2, double y2) {
-        Graphics2D g2d = (Graphics2D) g;
-
-        Polygon arrowHead = new Polygon();
-        arrowHead.addPoint(0, 5);
-        arrowHead.addPoint(-5, -5);
-        arrowHead.addPoint(5, -5);
-
-        double angle = Math.atan2(y2 - y1, x2 - x1);
-        g2d.translate(x2, y2);
-        g2d.rotate((angle - Math.PI / 2d));
-
-        g2d.fill(arrowHead);
-
-        g2d.rotate(-(angle - Math.PI / 2d));
-        g2d.translate(-x2, -y2);
-    }
 
     MooreEdge(MooreNode startNode, MooreNode endNode, String input) {
         super(startNode, endNode);
@@ -48,57 +27,19 @@ public class MooreEdge extends Edge {
         this.input = input;
     }
 
-    private int[] getTextPosition() {
-
-        double sx = startNode.getX();
-        double sy = startNode.getY();
-        double ex = endNode.getX();
-        double ey = endNode.getY();
-
-        double xDiff = sx - ex;
-        double yDiff = sy - ey;
-
-        double dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-
-        int x = (int) (sx - (dist / 2 * xDiff) / dist);
-        int y = (int) (sy - (dist / 2 * yDiff) / dist);
-
-        int[] point = { x, y };
-        return point;
-
-    }
-
-    public void move(double x, double y) {
-        startNode.move(x, y);
-        endNode.move(x, y);
-    }
-
     @Override
     void draw(Graphics g) {
         super.draw(g);
 
-        double[] startPoint = getStartPoint();
-        double[] endPoint = getEndPoint();
+        Font font = new Font("TimesRoman", Font.BOLD, 20);
 
-        double x1 = startPoint[0];
-        double y1 = startPoint[1];
-        double x2 = endPoint[0];
-        double y2 = endPoint[1];
-
-        drawArrowHead(g, x1, y1, x2, y2);
-
-        Font font = new Font("TimesRoman", Font.BOLD, 20 );
-
-        int centerX = getTextPosition()[0];
-        int centerY = getTextPosition()[1];
+        int tx = (int) getPeakPos()[0];
+        int ty = (int) getPeakPos()[1];
         FontMetrics metrics = g.getFontMetrics(font);
 
-        g.setColor(Color.WHITE);
-        g.fillOval(centerX - 20, centerY -20, 40, 40);
         g.setFont(font);
         g.setColor(Color.BLACK);
-        g.drawString(input, centerX - metrics.stringWidth(input) / 2, centerY + metrics.getAscent() / 2);
-
+        g.drawString(input, tx - metrics.stringWidth(input) / 2, ty + metrics.getAscent() / 2);
     }
 
     @Override
